d8c6ba4e30fcc2df9ff5690054f5ad10355643d4
matutalo <fabianaguntert@gmail.com>
2026-06-23 14:59:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-class armazenamento {
+const botao = document.getElementById("calcular");
 
-}
+botao.addEventListener("click", () => {
+    const custo = Number(document.getElementById("custo").value);
+    const tempo = document.getElementById("tempo").value;
+    const [horas, minutos] = tempo.split(":").map(Number);
 
-class perfil {
+    const tempoEmHoras = horas + minutos / 60;
 
-}
+    console.log(tempoEmHoras);
 
-class calculo {
+    const valorHora = Number(document.getElementById("valorhora").value);
+    const lucro = Number(document.getElementById("lucro").value);
+    const maoDeObra = tempoEmHoras * valorHora;
+    const valorBase = custo + maoDeObra;
+    const valorFinal = valorBase * (1 + lucro / 100);
 
-}
-
+    document.getElementById("resultado").textContent =
+        `Preço sugerido: R$ ${valorFinal.toFixed(2)}`;
+    
+});
